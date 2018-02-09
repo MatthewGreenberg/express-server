240d09e9341ace8cd43ffeb47677be12cf16c529
Matthew Greenberg <mattcgreenberg@gmail.com>
2018-02-08 20:40:42 -0600

    finish google oauth front end logic

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './header.css';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+
+  handleGoogleAuthClick() {
+    console.log(this.props);
+  }
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <a
+            href='/auth/google'
+            className='header__button'
+          >
+            Sign In With Google
+          </a>
+        );
+      default:
+        return (
+          <a
+            href='/api/logout'
+            className='header__button'
+          >
+            Log Out
+          </a>
+        )
+    }
+  }
   render() {
+    console.log(this.props.auth);
     return (
       <div className='header'>
+        {this.renderContent()}
         <h1 className='header__title'>Quiggly</h1>
-        <button className='header__button'>Sign In With Google</button>
+
       </div>
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return {auth}
+}
+
+export default connect(mapStateToProps)(Header);

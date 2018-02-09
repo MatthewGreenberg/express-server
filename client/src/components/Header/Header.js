f943724a18878fe66dfde63ce25b4db604d66c03
Matthew Greenberg <mattcgreenberg@gmail.com>
2018-02-08 18:06:05 -0600

    style header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,8 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className='header'>
-        Header
+        <h1 className='header__title'>Quiggly</h1>
+        <button className='header__button'>Sign In With Google</button>
       </div>
     );
   }

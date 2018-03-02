00a48893db099c3cfae8d30be72f918375bd0077
Matthew Greenberg <mattcgreenberg@gmail.com>
2018-03-01 18:15:51 -0600

    finish webhook logic with sendgrid

--- services/emailTemplates/surveyTemplates.js
+++ services/emailTemplates/surveyTemplates.js
@@ -4,15 +4,27 @@ module.exports = (survey) => {
   return `
     <html>
       <body>
-        <div style="text-align: center;">
+        <div style="text-align: center; background: lightblue; padding: 3em;">
           <h3>I'd like your input!</h3>
           <p>Please answer the following question:<p>
           <p>${survey.body}</p>
-          <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
-          </div>
-          <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+          <div style="margin-bottom: 20px">
+            <div style="margin-right: 10px; display: inline-block">
+              <a
+                href="${keys.redirectDomain}/api/surveys/${survey.id}/yes"
+                style="text-decoration: none"
+              >
+                Yes
+              </a>
+            </div>
+            <div style="display: inline-block">
+              <a
+                href="${keys.redirectDomain}/api/surveys/${survey.id}/no"
+                style="text-decoration: none"
+              >
+                No
+              </a>
+            </div>
           </div>
         </div>
       </body>
